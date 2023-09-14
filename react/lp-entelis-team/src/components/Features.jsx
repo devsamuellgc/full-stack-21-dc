f3044c404d884d851f9c75ae7e78713022132513
devsamuellgc <samuelgalrao@hotmail.com>
2023-09-14 19:33:20 -0300

    Ajuste do readME

--- react/lp-entelis-team/src/components/Features.jsx
+++ react/lp-entelis-team/src/components/Features.jsx
@@ -1,23 +1,26 @@
+import { useState } from "react";
 import Cards from "./Cards";
 
+const cards = [
+  {
+    title: "PERSONAL MANAGER",
+    text: "Available in the Pacific Time Zone working hours",
+    src: "https://static.tildacdn.com/tild3236-3339-4231-b161-366365366237/ill-e-1.svg",
+  },
+  {
+    title: "CLEAR BUDGET",
+    text: "We provide detailed estimates. You agree to everything before we start",
+    src: "https://static.tildacdn.com/tild6138-3037-4361-b039-323039383662/ill-e-2.svg",
+  },
+  {
+    title: "REALTIME DASHBOARD COMING SOON",
+    text: "Integrated dashboard providing full control over the development process",
+    src: "https://static.tildacdn.com/tild6366-6135-4966-b635-336633336561/ill-e-3.svg",
+  },
+];
+
 export default function Features() {
-  const cards = [
-    {
-      title: "PERSONAL MANAGER",
-      text: "Available in the Pacific Time Zone working hours",
-      src: "https://static.tildacdn.com/tild3236-3339-4231-b161-366365366237/ill-e-1.svg",
-    },
-    {
-      title: "CLEAR BUDGET",
-      text: "We provide detailed estimates. You agree to everything before we start",
-      src: "https://static.tildacdn.com/tild6138-3037-4361-b039-323039383662/ill-e-2.svg",
-    },
-    {
-      title: "REALTIME DASHBOARD COMING SOON",
-      text: "Integrated dashboard providing full control over the development process",
-      src: "https://static.tildacdn.com/tild6366-6135-4966-b635-336633336561/ill-e-3.svg",
-    },
-  ];
+  const [data, setData] = useState(cards);
 
   const card = {
     title: "REALTIME DASHBOARD COMING SOON",
@@ -26,14 +29,14 @@ export default function Features() {
   };
 
   function addCard() {
-    // chamar o array e enviar o card para o fim do array
+    setData([...data, card]);
   }
 
   return (
     <section className="flex items-center justify-between gap-12">
       <h2>Key features</h2>
-      <Cards data={cards} />
-      <button onClick={addCard()}>Adicionar</button>
+      <Cards data={data} />
+      <button onClick={addCard}>Adicionar</button>
     </section>
   );
 }
