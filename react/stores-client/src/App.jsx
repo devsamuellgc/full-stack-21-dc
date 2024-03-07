6f20409252b3d61f7b5e164474b8b6c642464285
devsamuellgc <samuelgalrao@hotmail.com>
2024-03-07 17:55:54 -0300

    feat: Instalação e primeiros testes com cypress

--- react/stores-client/src/App.jsx
+++ react/stores-client/src/App.jsx
@@ -1,9 +1,15 @@
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 
 export default function App() {
+  const [name, setName] = useState("");
+
+  function handleNewName(newName) {
+    setName(newName);
+  }
+
   async function fetchStores() {
     try {
       const stores = await axios.get("http://localhost:3000/stores/1513");
@@ -19,7 +25,7 @@ export default function App() {
         transition: Bounce,
       });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       toast.error(error.response.data.mensagem, {
         position: "top-right",
         autoClose: 5000,
@@ -34,8 +40,18 @@ export default function App() {
     }
   }
 
-  useEffect(() => {
-    fetchStores();
-  }, []);
-  return <ToastContainer />;
+  // useEffect(() => {
+  //   fetchStores();
+  // }, []);
+  return (
+    <div>
+      <h1 data-cy="title">Um texto para testar o teste</h1>
+      <p data-cy="initial-text">{name}</p>
+      <button data-cy="change-name" onClick={() => handleNewName("Samuel")}>
+        Mudar o nome
+      </button>
+      <p data-cy="final-text">{name}</p>
+      <ToastContainer />
+    </div>
+  );
 }
---
+++ react/stores-client/src/useApp.jsx
@@ -0,0 +1,15 @@
+export function useApp() {
+  let name = "";
+
+  function handleNewName(newName) {
+    name = newName;
+
+    return name;
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  return { sum, handleNewName, name };
+}
