7009a056205f21c860ad526b9d949fa21bd4f880
devsamuellgc <samuelgalrao@hotmail.com>
2024-03-21 18:50:21 -0300

    criação das configurações do jest

---
+++ api/test/index.test.js
@@ -0,0 +1,9 @@
+import { app } from "../src";
+import request from "supertest";
+
+describe("Users controller", () => {
+  it("Deve ser possível buscar as informações de todos os usuários cadastrados", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+  });
+});
